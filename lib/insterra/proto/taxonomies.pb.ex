5c4275a4d141f5a8d6212db1abbdd5f019fbbc41
Zack Siri <me@zacksiri.dev>
2023-07-27 16:02:13 +0700

    setup initial protos

---
+++ lib/insterra/proto/taxonomies.pb.ex
@@ -0,0 +1,8 @@
+defmodule Insterra.Proto.Taxonomies.Category do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :id, 1, type: :int32
+  field :name, 2, type: :string
+end
