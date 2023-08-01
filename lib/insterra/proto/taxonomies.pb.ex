0634dde655a0a4215ba5670dfb1ea6540b7e8a38
Zack Siri <me@zacksiri.dev>
2023-08-01 09:18:35 +0700

    update taxonomies

--- lib/insterra/proto/taxonomies.pb.ex
+++ lib/insterra/proto/taxonomies.pb.ex
@@ -15,6 +15,7 @@ defmodule Insterra.Proto.Taxonomies.Category do
   field(:name, 2, type: :string)
   field(:rank, 3, type: :int32)
   field(:configurable, 4, type: :bool)
+  field(:parent_id, 5, type: :int32, json_name: "parentId")
 end
 
 defmodule Insterra.Proto.Taxonomies.Handler.Service do
