b6b201217c3280244e17a1e0c640a9caf3d76217
Zack Siri <me@zacksiri.dev>
2023-08-15 14:18:47 +0700

    refactor responses

--- lib/insterra/protos/configurations.pb.ex
+++ lib/insterra/protos/configurations.pb.ex
@@ -1,12 +1,3 @@
-defmodule Insterra.Protos.Configurations.Status do
-  @moduledoc false
-
-  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
-
-  field :code, 1, type: Insterra.Protos.Responses.Code, enum: true
-  field :message, 2, type: :string
-end
-
 defmodule Insterra.Protos.Configurations.Archive.CreateRequest do
   @moduledoc false
 
@@ -24,7 +15,7 @@ defmodule Insterra.Protos.Configurations.Archive.Response do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :status, 1, type: Insterra.Protos.Configurations.Status
+  field :status, 1, type: Insterra.Protos.Responses.Status
   field :data, 3, type: Insterra.Protos.Configurations.Archive
 end
 
@@ -51,7 +42,7 @@ defmodule Insterra.Protos.Configurations.Archive do
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field :id, 1, type: :int32
+  field :id, 1, type: :string
   field :files, 2, repeated: true, type: Insterra.Protos.Configurations.Archive.File
 end
 
--- lib/insterra/protos/responses.pb.ex
+++ lib/insterra/protos/responses.pb.ex
@@ -6,3 +6,12 @@ defmodule Insterra.Protos.Responses.Code do
   field :OK, 0
   field :NOT_FOUND, 1
 end
+
+defmodule Insterra.Protos.Responses.Status do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field :code, 1, type: Insterra.Protos.Responses.Code, enum: true
+  field :message, 2, type: :string
+end
