cfe7ac8d957bcca1b2bfbd95b6f8ed538afd74c1
Zack Siri <me@zacksiri.dev>
2023-08-16 16:56:04 +0700

    update platform provider

--- lib/insterra/protos/platforms.pb.ex
+++ lib/insterra/protos/platforms.pb.ex
@@ -1,36 +1,7 @@
-defmodule Insterra.Protos.Platforms.Provider.ListRequest do
-  @moduledoc false
-
-  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
-
-  field(:query, 1, type: :string)
-end
-
 defmodule Insterra.Protos.Platforms.Provider do
   @moduledoc false
 
   use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
 
-  field(:id, 1, type: :int32)
-  field(:name, 2, type: :string)
-  field(:source, 3, type: :string)
-  field(:version, 4, type: :string)
-end
-
-defmodule Insterra.Protos.Platforms.Handler.Service do
-  @moduledoc false
-
-  use GRPC.Service, name: "insterra.protos.platforms.Handler", protoc_gen_elixir_version: "0.12.0"
-
-  rpc(
-    :ListProviders,
-    Insterra.Protos.Platforms.Provider.ListRequest,
-    stream(Insterra.Protos.Platforms.Provider)
-  )
-end
-
-defmodule Insterra.Protos.Platforms.Handler.Stub do
-  @moduledoc false
-
-  use GRPC.Stub, service: Insterra.Protos.Platforms.Handler.Service
+  field(:name, 1, type: :string)
 end
