0cd4cefe3de6d4e8d78aad6a4d2a381a5b543a79
Zack Siri <me@zacksiri.dev>
2023-11-15 14:03:40 +0700

    add ability to register metric and expose category limit

--- lib/insterra/protos/accounts.pb.ex
+++ lib/insterra/protos/accounts.pb.ex
@@ -16,6 +16,15 @@ defmodule Insterra.Protos.Accounts.UserReference do
   field(:uid, 2, type: :string)
 end
 
+defmodule Insterra.Protos.Accounts.Organization.Metric do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field(:category_name, 1, type: :string)
+  field(:count, 2, type: :int32)
+end
+
 defmodule Insterra.Protos.Accounts.Organization.Registration do
   @moduledoc false
 
@@ -24,6 +33,7 @@ defmodule Insterra.Protos.Accounts.Organization.Registration do
   field(:name, 1, type: :string)
   field(:provider, 2, type: :string)
   field(:uid, 3, type: :string)
+  field(:metrics, 4, repeated: true, type: Insterra.Protos.Accounts.Organization.Metric)
 end
 
 defmodule Insterra.Protos.Accounts.Organization.Response do
--- lib/insterra/protos/taxonomies.pb.ex
+++ lib/insterra/protos/taxonomies.pb.ex
@@ -6,6 +6,16 @@ defmodule Insterra.Protos.Taxonomies.Category.ListRequest do
   field(:configurable, 1, repeated: true, type: :bool)
 end
 
+defmodule Insterra.Protos.Taxonomies.Category.Limit do
+  @moduledoc false
+
+  use Protobuf, protoc_gen_elixir_version: "0.12.0", syntax: :proto3
+
+  field(:min, 1, type: :int32)
+  field(:max, 2, type: :int32)
+  field(:per, 3, type: :string)
+end
+
 defmodule Insterra.Protos.Taxonomies.Category do
   @moduledoc false
 
@@ -17,6 +27,7 @@ defmodule Insterra.Protos.Taxonomies.Category do
   field(:configurable, 4, type: :bool)
   field(:description, 5, type: :string)
   field(:parent_id, 6, type: :int32)
+  field(:limits, 7, repeated: true, type: Insterra.Protos.Taxonomies.Category.Limit)
 end
 
 defmodule Insterra.Protos.Taxonomies.Tag do
